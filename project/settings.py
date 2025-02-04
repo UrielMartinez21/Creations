00c651bd59bc6058b61fe787317d68f17b965cf8
UrielMartinez21 <uriellmartinezz35@gmail.com>
2025-02-04 00:57:57 -0600

    updated all project

--- project/settings.py
+++ project/settings.py
@@ -64,7 +64,9 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -132,6 +134,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # This production code might break development mode, so we check whether we're in DEBUG mode
 if not DEBUG:
