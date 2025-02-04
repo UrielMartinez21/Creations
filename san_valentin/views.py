122f5851561a22f2b57fa2e234c6f025d12f47ed
UrielMartinez21 <uriellmartinezz35@gmail.com>
2025-02-03 23:58:34 -0600

    updated all

--- san_valentin/views.py
+++ san_valentin/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 
 
 def main_index(request):
-    return render(request, 'san_valentin/main_index.html')
+    return render(request, 'main_index.html')
 
 
 def index_v1(request):
-    return render(request, 'san_valentin/index_v1.html')
+    return render(request, 'index_v1.html')
