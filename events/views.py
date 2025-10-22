c8c37a505e98b511ccd1f60e69ff2e0b514841e9
UrielMartinez21 <uriellmartinezz35@gmail.com>
2025-10-22 08:08:58 -0600

    feat: removed dentist section of all project

--- events/views.py
+++ events/views.py
@@ -12,10 +12,5 @@ def valentine_v1(request: HttpRequest) -> HttpResponse:
     return render(request, "valentine/v1.html", {"name": name})
 
 
-def dentist(request: HttpRequest) -> HttpResponse:
-    name = request.GET.get("name", "")
-    return render(request, "dentist/v1.html", {"name": name})
-
-
 def flower_garden(request: HttpRequest) -> HttpResponse:
     return render(request, "flowers/flower_garden.html")
