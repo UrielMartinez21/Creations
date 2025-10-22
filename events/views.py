dae8bece676905cda9b34d078e29a6c3c3c51ed0
UrielMartinez21 <uriellmartinezz35@gmail.com>
2025-10-22 07:47:33 -0600

    chore: added type hinting in view

--- events/views.py
+++ events/views.py
@@ -1,19 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 
 
-def yellow_flowers(request):
-    name = request.GET.get('name', '')
-    return render(request, 'flowers/yellow_flowers.html', {'name': name})
+def yellow_flowers(request: HttpRequest) -> HttpResponse:
+    name = request.GET.get("name", "")
+    return render(request, "flowers/yellow_flowers.html", {"name": name})
 
 
-def valentine_v1(request):
-    name = request.GET.get('name', '')
-    return render(request, 'valentine/v1.html', {'name': name})
+def valentine_v1(request: HttpRequest) -> HttpResponse:
+    name = request.GET.get("name", "")
+    return render(request, "valentine/v1.html", {"name": name})
 
 
-def dentist(request):
-    name = request.GET.get('name', '')
-    return render(request, 'dentist/v1.html', {'name': name})
+def dentist(request: HttpRequest) -> HttpResponse:
+    name = request.GET.get("name", "")
+    return render(request, "dentist/v1.html", {"name": name})
 
-def flower_garden(request):
-    return render(request, 'flowers/flower_garden.html')
+
+def flower_garden(request: HttpRequest) -> HttpResponse:
+    return render(request, "flowers/flower_garden.html")
